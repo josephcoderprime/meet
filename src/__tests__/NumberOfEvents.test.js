71a6a502e06d50911a448be0dbd62a3871cb3a7e
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-06-14 13:01:14 +0100

    integration tests for the NumberOfEvents component

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import NumberofEvents from '../NumberofEvents';
+import NumberOfEvents from '../components/NumberOfEvents/NumberOfEvents';
 
-describe('<NumberofEvents /> component', () => {
-
-  let numberFilter;
-
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper
   beforeAll(() => {
-    numberFilter = shallow(<NumberofEvents />);
-  });
+    NumberOfEventsWrapper = shallow(<NumberOfEvents numberOfEvents={32} updateNumberOfEvents={() => { }} />)
+  })
 
-  test('render the number of events menu', () => {
-    expect(numberFilter.find('.numberOfEvents')).toHaveLength(1);
-  });
+  test('should render the input to change the number of events', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
+  })
+
+  test('should render value 32 events by default', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEvents').prop('value')).toBe(32);
+  })
+
+  test('should change when someone change the number', () => {
+    const event = { target: { value: 10 } }
+    NumberOfEventsWrapper.find('.numberOfEvents').simulate('change', event)
+    expect(NumberOfEventsWrapper.find('.numberOfEvents').prop('value')).toBe(10);
+  })
+
 });
