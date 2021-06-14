be5a4be42c215c5fe08c622712613f859dd58261
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-06-14 12:58:46 +0100

    Modified the importation  + minor changes

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import EventList from '../EventList';
-import Event from '../Event';
-import { mockData } from '../mock-data';
+import EventList from '../components/event-list/EventList';
+import Event from '../components/event/Event';
+import { mockData } from '../mock-data/mock-data'
 
 describe('<EventList /> component', () => {
-  test('render correct number of events', () => {
+  test('render the correct number of events', () => {
     const EventListWrapper = shallow(<EventList events={mockData} />);
     expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
-  });
+  })
 });
