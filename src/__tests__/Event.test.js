7e4ecf8bc358d7881a00627d23376a3226ccf5a0
josephcoderprime <77362650+josephcoderprime@users.noreply.github.com>
2021-06-14 12:56:43 +0100

    Modified the Event.test.js

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Event from '../Event';
-import EventList from '../EventList';
-import { mockData } from '../mock-data';
+import Event from '../components/event/Event';
+import { mockData } from '../mock-data/mock-data'
 
-describe('<EventList /> component', () => {
-  let createdEvent;
+describe('<Event /> component', () => {
+  let event
+  let EventWrapper
   beforeAll(() => {
-    createdEvent = shallow(<Event event={mockData[0]} />)
-  });
+    event = mockData[0];
+    EventWrapper = shallow(<Event event={event} />)
+  })
 
-  test('render event list', () => {
-    const EventListWrapper = shallow(<EventList events={mockData} />);
-    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
-  });
+  test('should have a collapsed event details by default', () => {
+    expect(EventWrapper.find('.event__details')).toHaveLength(1);
+    expect(EventWrapper.find('.display-none')).toHaveLength(1);
+  })
 
-  test('render the event show more button', () => {
-    expect(createdEvent.find('.details-btn')).toHaveLength(1);
-  });
+  test('should show event details when click on button', () => {
+    EventWrapper.find('.event__button').simulate('click')
+    expect(EventWrapper.find('.display-none')).toHaveLength(0);
+  })
 
-  test('show more button should be false on render', () => {
-    expect(createdEvent.state('showMore')).toBe(false);
-  });
+  test('should hide event details when click on button', () => {
+    EventWrapper.state({
+      show: true
+    })
+    EventWrapper.find('.event__button').simulate('click')
+    expect(EventWrapper.find('.display-none')).toHaveLength(1);
+  })
 
-  test('if showMore is false, simulates a click showing more details', () => {
-    const changeState = createdEvent.state({ showMore: true });
-    createdEvent.find('.details-btn').simulate('click', changeState);
-    expect(createdEvent.state('showMore')).toBe(true);
-  });
-
-  test('if showMore is true, simulates a click showing less details', () => {
-    const changeState = createdEvent.state({ showMore: false });
-    createdEvent.find('.details-btn').simulate('click', changeState);
-    expect(createdEvent.state('showMore')).toBe(false);
-  });
+  test('should show all the data of the events', () => {
+    expect(EventWrapper.find('.event__summary')).toHaveLength(1);
+    expect(EventWrapper.find('.event__location')).toHaveLength(1);
+    expect(EventWrapper.find('.event__description')).toHaveLength(1);
+    expect(EventWrapper.find('.event__organizer')).toHaveLength(1);
+    expect(EventWrapper.find('.event__start')).toHaveLength(1);
+    expect(EventWrapper.find('.event__end')).toHaveLength(1);
+  })
 });
